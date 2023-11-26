a55a90ca38cfbdb6811dd88e4e9cb588a7ff0043
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-11-26 02:59:34 -0600

    compress

---
+++ get-sample.sh
@@ -0,0 +1,1 @@
+wget https://cdn.britannica.com/44/128144-050-1FB7865D/Pleiades.jpg
--- transformSymbol.py
+++ transformSymbol.py
@@ -5,15 +5,25 @@
 # Scale:
 # https://cloudinary.com/guides/bulk-image-resize/python-image-resize-with-pillow-and-opencv
 
-# AI
+# from: AI
 # https://chat.openai.com/share/7d24c939-08d3-4446-968e-61b397fc1dcd
 # https://chat.openai.com/c/fc923815-b652-48e9-b166-c318ea348d58
+
+# Doc
+# https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#jpeg
+
+# Quality
+# sch: https://www.google.com/search?q=python+pillow+save+jpeg+size, https://www.google.com/search?q=python+pillow+save+jpeg+smaller
+# https://jdhao.github.io/2019/07/20/pil_jpeg_image_quality/
+
+# OpenCV
+# https://cloudinary.com/guides/bulk-image-resize/python-image-resize-with-pillow-and-opencv
 
 from pathlib import Path
 from PIL import Image # Open a WebP image
 
 
-original = "1_rAO0XWvBPdETsDNbqvi_dA.webp"
+original = "Pleiades.webp"
 name = original.split('.')[0]
 
 
@@ -24,7 +34,7 @@ def convertToPng():
 
     try:
         png_image = webp_image.convert("RGBA")
-        png_image.save(f"{name}.png")
+        png_image.save(f"{name}.png", optimize=True, quality=100)
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -47,5 +57,5 @@ def scale_image(input_path, output_path, scale_percent):
 output_image_path = f"scaled_{name}.png"
 scale_percent = 50  # Change this to the desired percentage
 
-scale_image(original, output_image_path, scale_percent)
 convertToPng()
+#scale_image(original, output_image_path, scale_percent)
