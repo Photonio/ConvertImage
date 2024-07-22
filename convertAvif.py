e981a283e0c4cbbd50603777dfcc373d47f287ee
DarkStar⬢🕋🪐☀ <darkstar@blackcubeai.com>
2024-07-21 22:55:07 -0400

    AVIF

---
+++ convertAvif.py
@@ -0,0 +1,4 @@
+# Todo:
+# https://www.google.com/search?q=python+convert+avif
+# https://www.google.com/search?q=python+convert+avif+to+jpg
+# https://codecalamity.com/using-avif-and-heif-images-with-python-pil/
